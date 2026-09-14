7bc0e2bcecdb58a1397536e87f4d4113963844d1
seo-dongyeon <s.dongyeon.me@gmail.com>
2026-09-14 13:38:30 +0900

    solve Q2_A_LL

--- Linked_List/Q2_A_LL.c
+++ Linked_List/Q2_A_LL.c
@@ -109,16 +109,17 @@ void alternateMergeLinkedList(LinkedList *ll1, LinkedList *ll2)
 	ListNode *next1;
 	ListNode *next2;
 
-	while (cur1 != NULL)
+	while (cur1 != NULL && cur2 != NULL)
 	{
 		next1 = cur1->next;
-		cur1->next = cur2->item;
 		next2 = cur2->next;
-		removeNode(ll2, 0);
+		cur1->next = cur2;
+		cur2->next = next1;
 		cur2 = next2;
 		cur1 = next1;
 
 	}
+	ll2->head = cur2;
 }
 
 ///////////////////////////////////////////////////////////////////////////////////
