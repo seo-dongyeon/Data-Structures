9aad6b24fd556dcb4b7e32e79adbd3411099fa7a
seo-dongyeon <s.dongyeon.me@gmail.com>
2026-09-17 11:16:58 +0900

    solve Q7_A_LL

--- Linked_List/Q7_A_LL.c
+++ Linked_List/Q7_A_LL.c
@@ -88,6 +88,15 @@ int main()
 void RecursiveReverse(ListNode **ptrHead)
 {
 	/* 여기에 코드를 작성하세요 */
+	if (ptrHead == NULL || *ptrHead == NULL || (*ptrHead)->next == NULL) return;
+
+	ListNode *first = *ptrHead;
+    ListNode *rest = first->next;
+
+	RecursiveReverse(&rest);
+	first->next->next = first;
+	first->next = NULL;
+	*ptrHead = rest;
 }
 
 //////////////////////////////////////////////////////////////////////////////////
